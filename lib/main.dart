8e2b47f74402d54eed3f8b630387407c8d0beafc
Varenya Tiwari <varenya.tiwari@iitg.ac.in>
2021-10-22 15:39:19 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp();
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Aadhar Address App"),
+    );
+  }
+}
